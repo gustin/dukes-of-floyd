61cef0423b9d431628b7894ceaefa4031776b699
unknown <Owner@.(none)>
2010-02-24 13:13:13 -0500

    created new el camino car and startscreen

---
+++ src/inputs/intro_input.rb
@@ -0,0 +1,11 @@
+Jemini::InputBuilder.declare do |i|
+
+  i.in_order_to :start do
+	i.release :down_arrow
+  
+  end
+
+  i.in_order_to :quit do
+	i.release :q
+  end
+end
--- src/states/intro_state.rb
+++ src/states/intro_state.rb
@@ -1,11 +1,11 @@
 class IntroState < Jemini::GameState
 
   def load
+     menu_handler = create :GameObject, :HandlesEvents
+  menu_handler.handle_event(:start) { switch_state :PlayState } 
     create :Background, :blue_ridge_mountains
     create :Text, 'Dukes of Floyd', 
            :position => Vector.new(screen_width, screen_height).half
   end
-
 end
-
 
