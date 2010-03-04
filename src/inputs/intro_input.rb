538230feeebb9d118fc73a35d7b02412de71e356
unknown <Owner@.(none)>
2010-03-04 12:58:45 -0500

    added road

--- src/inputs/intro_input.rb
+++ src/inputs/intro_input.rb
@@ -1,7 +1,10 @@
 Jemini::InputBuilder.declare do |i|
 
   i.in_order_to :start do
-    i.release :down_arrow
+	  i.release :down_arrow
   end
 
+  i.in_order_to :quit do
+	  i.release :q
+  end
 end
