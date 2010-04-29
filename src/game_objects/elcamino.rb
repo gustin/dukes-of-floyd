143c41aad851754f5afb49efbfa971ed550fe61a
unknown <Owner@.(none)>
2010-04-29 15:07:23 -0400

    we added edges to both sides of the screen to stop from going out of sight.

--- src/game_objects/elcamino.rb
+++ src/game_objects/elcamino.rb
@@ -1,21 +1,30 @@
 class Elcamino < Jemini::GameObject
-  MOVEMENT_RATE = 0.5 
+  attr_accessor :movement_rate
+  EDGE_OF_LEFT_SCREEN = 29.0
   has_behavior :DrawableImage
   has_behavior :HandlesEvents
 
   def load
     self.image = :elcamino
+    @movement_rate = 0.5 
     handle_event :move_left do |message|
-      if position.x > 29.0 
-        position.x -= MOVEMENT_RATE * message.delta 
-      end  
+      if position.x > EDGE_OF_LEFT_SCREEN
+        position.x -= @gameobject.movement_rate * message.delta 
+        puts position
+      end
+      if position.x == 299.0 
+       @game_object.movement_rate = 0.1   
+      end
     end
     handle_event :move_right do |message|
       if position.x < self.game_state.screen_size.x
-        position.x += MOVEMENT_RATE * message.delta
+        position.x += @movement_rate * message.delta
+        puts position
+      end  
+      if position.x == 472.5 
+       @movement_rate = 0.0
       end
     end
   end
-  
 end
 
