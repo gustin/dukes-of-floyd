d7bcc8e533641bc530141a4864cd3fc9044ae345
gustin <gustin@entryway.net>
2010-04-29 15:28:50 -0400

    supports speeding the velocity back up after road edges are encountered

--- src/game_objects/elcamino.rb
+++ src/game_objects/elcamino.rb
@@ -1,20 +1,27 @@
 class Elcamino < Jemini::GameObject
   attr_accessor :movement_rate
+  SLOW_SPEED = 0.1
+  NORMAL_SPEED = 0.5
+  EDGE_OF_LEFT_ROAD = 299.0
+  EDGE_OF_RIGHT_ROAD = 472.50
   EDGE_OF_LEFT_SCREEN = 29.0
   has_behavior :DrawableImage
   has_behavior :HandlesEvents
 
   def load
     self.image = :elcamino
-    @movement_rate = 0.5 
+    @movement_rate = NORMAL_SPEED
     elcamino = self
     handle_event :move_left do |message|
       puts position
       if position.x > EDGE_OF_LEFT_SCREEN
         position.x -= elcamino.movement_rate * message.delta 
       end
-      if position.x == 299.0 
-       elcamino.movement_rate = 0.1   
+      if position.x <= EDGE_OF_LEFT_ROAD
+       elcamino.movement_rate = SLOW_SPEED
+      end
+      if position.x > EDGE_OF_LEFT_ROAD
+        elcamino.movement_rate = NORMAL_SPEED
       end
     end
     handle_event :move_right do |message|
@@ -22,8 +29,11 @@ class Elcamino < Jemini::GameObject
       if position.x < self.game_state.screen_size.x
         position.x += elcamino.movement_rate * message.delta
       end  
-      if position.x == 472.5 
-        elcamino.movement_rate = 0.1
+      if position.x >= EDGE_OF_RIGHT_ROAD
+        elcamino.movement_rate = SLOW_SPEED
+      end
+      if position.x < EDGE_OF_RIGHT_ROAD
+        elcamino.movement_rate = NORMAL_SPEED
       end
     end
   end
